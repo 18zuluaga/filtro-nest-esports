05e566d0b71ad6bc8b9299478c0c80eeda281895
18zuluaga <18zuluaga1989@gmail.com>
2024-10-07 16:22:35 -0400

    feat(database): add initial database connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { TournamentsModule } from './tournaments/tournaments.module';
 import { PlayersModule } from './players/players.module';
 import { ResultsModule } from './results/results.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tournament } from './tournaments/entities/tournament.entity';
+import { Player } from './players/entities/player.entity';
+import { Result } from './results/entities/result.entity';
 
 @Module({
-  imports: [TournamentsModule, PlayersModule, ResultsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Tournament, Player, Result],
+      synchronize: true,
+    }),
+    TournamentsModule,
+    PlayersModule,
+    ResultsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
